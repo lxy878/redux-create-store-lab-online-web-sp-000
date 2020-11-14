5a3a58d46aae9357fadc3acafd92927af5e5a1d1
Xiaoyi Liu <lxy878@gmail.com>
2020-11-14 02:10:05 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
-// write your createStore function here
+function createStore(reducer){
+  let state
+
+  function dispatch(action){
+    state = reducer(state, action)
+    render()
+  }
+
+  function getState(){
+    return state
+  }
+
+  return
+}
 
 function candyReducer(state = [], action) {
   switch (action.type) {
